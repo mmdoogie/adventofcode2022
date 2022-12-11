5e0f1ff499b73cc7c508be3a3b81cd345cf2a148
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2022-12-10 23:54:48 -0600

    Day 11

---
+++ 11/day11.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+from aocd import lines
+
+monkeys = []
+for n in range(8):
+	m = {}
+	m["items"] = [int(x) for x in lines[n*7+1].split(":")[1].split(",")]
+	ops = lines[n*7+2].split("= ")[1].split(" ")
+	if ops[2] != "old":
+		m["opv"] = int(ops[2])
+		if ops[1] == "*":
+			m["op"] = lambda x, y: x * y
+		else:
+			m["op"] = lambda x, y: x + y
+	else:
+		m["opv"] = 0
+		m["op"] = lambda x, y: x * x
+	m["tdiv"] = int(lines[n*7+3].split("by ")[1])
+	m["dt"] = int(lines[n*7+4].split("monkey ")[1])
+	m["df"] = int(lines[n*7+5].split("monkey ")[1])
+	m["count"] = 0
+
+	monkeys += [m]
+
+for n in range(20):
+	for m in monkeys:
+		for i in m["items"]:
+			m["count"] += 1
+			worry = i
+			worry = m["op"](worry, m["opv"])
+			worry = int(worry / 3)
+			if worry % m["tdiv"] == 0:
+				monkeys[m["dt"]]["items"] += [worry]
+			else:
+				monkeys[m["df"]]["items"] += [worry]
+		m["items"] = []
+
+cnts = [m["count"] for m in monkeys]
+cnts.sort(reverse=True)
+print(cnts[0] * cnts[1])
