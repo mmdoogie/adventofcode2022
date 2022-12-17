aef719e647733a71c81babfba2021cab7e752d05
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2022-12-17 01:54:21 -0600

    ~8x speedup on Day 16

--- 16/day16.py
+++ 16/day16.py
@@ -1,67 +1,91 @@
 #!/bin/python3
 
 from aocd import lines
-import copy
+import re
+import itertools
 
 allValves = {}
+rx = re.compile("Valve ([A-Z]{2}) .*=([0-9]+); .*valve[s]? ([A-Z, ]+)")
 for l in lines:
-	n = l.split(" ")[1]
-	r = int(l.split("=")[1].split(";")[0])
-	d = l.split(" ",9)[9].split(", ")
-	allValves[n] = {"name": n, "rate": r, "dest": d, "open": False, "dist": 1000, "visited": False}
+	g = rx.search(l)
+	name = g[1]
+	rate = int(g[2])
+	dests = g[3].split(", ")
+	allValves[name] = {"rate": rate, "dest": dests, "dist": 1000, "visited": False}
 
 def doDistancing(valves, startPos):
 	for k,v in valves.items():
 		v["dist"] = 1000
 		v["visited"] = False
 	valves[startPos]["dist"] = 0
-	
+
 	currPos = startPos
 	while True:
+		newDist = valves[currPos]["dist"] + 1
 		for n in valves[currPos]["dest"]:
 			if valves[n]["visited"]:
 				continue
-			newDist = valves[currPos]["dist"] + 1
-			if newDist < valves[n]["dist"]:
-				valves[n]["dist"] = newDist
+			valves[n]["dist"] = min(valves[n]["dist"], newDist)
 
 		valves[currPos]["visited"] = True
-		remDist = [v["dist"] for k,v in valves.items() if not v["visited"]]
-		if len(remDist) == 0:
+		rem = sorted([(k, v["dist"]) for k,v in valves.items() if not v["visited"] and v["dist"] < 1000], key=lambda x: x[1])
+		if not rem:
 			break
-		minRem = min(remDist)
-		if minRem == 1000:
-			break
-		currPos = [v for k,v in valves.items() if v["dist"] == minRem and not v["visited"]][0]["name"]
+		nextPos, minDist = rem[0]
+		currPos = nextPos
+
+	dists = []
+	for k,v in valves.items():
+		if v["rate"] == 0 or k == startPos:
+			continue
+		dists += [(k, v["dist"], v["rate"])]
+
+	return dists
 
 allDist = {}
+for k,v in allValves.items():
+	if v["rate"] == 0 and k != "AA":
+		continue
+	allDist[k] = doDistancing(allValves, k)
 
-paths = [["AA"]]
-times = [30]
-flows = [0]
+ptfs = [(["AA"], 30, 0)]
 while True:
-	newPaths = []
-	newTimes = []
-	newFlows = []
-	for p,t,f in zip(paths, times, flows):
-		here = p[-1]
-		if here not in allDist:
-			doDistancing(allValves, here)
-			allDist[here] = copy.deepcopy(allValves)
-		valves = allDist[here]
-		choices = []
-		for k,v in valves.items():
-			score = (t-v["dist"]-1)*v["rate"]
-			if v["rate"] != 0 and v["dist"] != 0 and score > 0 and not v["name"] in p:
-				choices += [(k, t - v["dist"] - 1, v["rate"])]
-		if len(choices) == 0:
-			continue
+	newPtfs = []
+	for p, t, f in ptfs:
+		valves = allDist[p[-1]]
+		choices = [(v[0], t - v[1] - 1, v[2]) for v in valves if t - v[1] > 1 and v[0] not in p]
 		for c in choices:
-			newPaths += [p + [c[0]]]
-			newTimes += [c[1]]
-			newFlows += [f + c[1]*c[2]]
-	if len(newPaths) == 0:
+			newPtfs += [(p + [c[0]], c[1], f + c[1] * c[2])]
+	if not newPtfs:
 		break
-	paths, times, flows = newPaths, newTimes, newFlows
+	prevPtfs = ptfs
+	ptfs = newPtfs
 
-print(max(flows))
+ptfs.sort(key=lambda x: x[2], reverse=True)
+print(ptfs[0][2])
+
+ptfs = [(["AA"], 26, 0)]
+while True:
+	newPtfs = []
+	for p, t, f in ptfs:
+		valves = allDist[p[-1]]
+		choices = [(v[0], t - v[1] - 1, v[2]) for v in valves if t - v[1] > 1 and v[0] not in p]
+		for c in choices:
+			newPtfs += [(p + [c[0]], c[1], f + c[1] * c[2])]
+	if not newPtfs:
+		break
+	prevPtfs = ptfs
+	ptfs = newPtfs
+
+rs = []
+for ptf in prevPtfs + ptfs:
+	rs += [(set(ptf[0][1:]), ptf[2])]
+
+fmax = 0
+for pfm, pfe in itertools.product(rs, rs):
+	if pfm[0] & pfe[0]:
+		continue
+	if pfm[1] + pfe[1] > fmax:
+		fmax = pfm[1] + pfe[1]
+
+print(fmax)
